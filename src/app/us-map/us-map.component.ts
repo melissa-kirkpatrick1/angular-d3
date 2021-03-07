47ea5f2fbdc790f82063043cae4db79385a07f19
unknown <melissa.kirkpatrick1@gmail.com>
2021-03-07 11:27:40 -0500

    good working plot of hotels

--- src/app/us-map/us-map.component.ts
+++ src/app/us-map/us-map.component.ts
@@ -85,84 +85,68 @@ export class UsMapComponent implements OnInit {
 
   }
   updateThreatFilters(event) {
-    let width = 900;
-    let height = 600;
-
+    console.log("Clicked event",this.selectedValues);
     let projection = d3.geoAlbersUsa();
     let path = d3.geoPath()
       .projection(projection);
 
-    // d3.select('div.us-map').html("");
-    // let svg = d3.select('div.us-map').append('svg')
-    //   .attr('width', width)
-    //   .attr('height', height);
-    // let g = svg.append('g');
-
     let svg = d3.select('div.us-map');
     let g = d3.select('g.map');
 
-    console.log("SVG", svg);
-    console.log("outside json calling1");
+    var myimage = svg.append('image')
+      .attr('xlink:href', 'http://lorempixel.com/200/200/')
+      .attr('width', 50)
+      .attr('height', 50)
+
+
     let tooltipDiv = d3.select("div.tooltip")
       .attr("class", "tooltip")
       .style("opacity", 0);
 
-    this.countryData
-      .then(function (topology:any) {
-        g.selectAll('path')
-          .data(t.feature(topology, topology.objects.units).features)
-          .enter()
-          .append('path')
-          .attr('d', path)
-          .attr("fill", function (d ) {
-            return "lightgrey"
-          });
-        console.log("ending json calling1");
-        let aa = [-122.490402, 37.786453];
-        let bb = [-75.26660601124958,43.598241844086594];
-        let cc = [-74.00712, 40.71455];
+    let aa = [-122.490402, 37.786453];
+    let bb = [-75.26660601124958,43.598241844086594];
+    let cc = [-74.00712, 40.71455];
 
-        let sampleData = {"location" : "CA Somewhere", "coords" : aa};
-        let sampleData2 = {"location" : "Herndon", "coords" : bb};
-        let sampleData3 = {"location" : "New York", "coords" : cc};
-        let dataArr : any[] = [];
-        dataArr.push(sampleData);
-        dataArr.push(sampleData2);
-        dataArr.push(sampleData3);
-        g.selectAll("circle")
-          .data(dataArr).enter()
-          .append("circle")
-          .attr("cx", function (d:any) {
-            var coords = projection(d.coords);
-            if (coords) {
-              return coords[0];
-            }
-          })
-          .attr("cy", function (d: any) {
-            console.log("D HERE 2",d);
-            var coords = projection(d.coords);
-            if (coords) {
-              return coords[1];
-            }
-          })
-          .attr("r", "5px")
-          .attr("fill", "red")
-          .attr("class", "point_on_map")
-          .on("mouseover", function(event, data) {
-            console.log("data", data);
-            tooltipDiv.transition()
-              .duration(200)
-              .style("opacity", .9);
-            tooltipDiv.html(data.location)
-              .style("left", (event.pageX) + "px")
-              .style("top", (event.pageY - 28) + "px");
-          })
-          .on("mouseout", function(d) {
-            tooltipDiv.transition()
-              .duration(500)
-              .style("opacity", 0);
-          });
-        console.log("LOADED 1 point - YAY");
+    let sampleData = {"location" : "CA Somewhere", "coords" : aa};
+    let sampleData2 = {"location" : "Herndon", "coords" : bb};
+    let sampleData3 = {"location" : "New York", "coords" : cc};
+    let dataArr : any[] = [];
+    dataArr.push(sampleData);
+    dataArr.push(sampleData2);
+    dataArr.push(sampleData3);
+    let size=50;
+    g.selectAll("rects")
+      .data(dataArr).enter()
+      .append("rect")
+      .attr("width", size)
+      .attr("height", size)
+      .attr("x", function (d:any) {
+        var coords = projection(d.coords);
+        if (coords) {
+          return coords[0];
+        }
+      })
+      .attr("y", function (d: any) {
+        var coords = projection(d.coords);
+        if (coords) {
+          return coords[1];
+        }
+      })
+      .attr("class", "hotel")
+      // .attr("fill", "#bg")
+      // .attr("fill", "http://simpleicon.com/wp-content/uploads/smile.png")
+      .on("mouseover", function(event, data) {
+        tooltipDiv.transition()
+          .duration(200)
+          .style("opacity", .9);
+        tooltipDiv.html(data.location)
+          .style("left", (event.pageX) + "px")
+          .style("top", (event.pageY - 28) + "px");
+      })
+      .on("mouseout", function(d) {
+        tooltipDiv.transition()
+          .duration(500)
+          .style("opacity", 0);
       });
 
   }
